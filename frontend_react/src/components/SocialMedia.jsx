3c2b9f7f664529e3366999031c8a11a0472e3b6a
Gerard Di Mattia <108446206+dimattiag@users.noreply.github.com>
2022-10-20 12:24:14 -0400

    inicial commit

---
+++ frontend_react/src/components/SocialMedia.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BsTwitter, BsGithub, BsLinkedin } from 'react-icons/bs';
+
+
+
+
+const SocialMedia = () => (
+    <div className="app__social">
+      <div>
+        <BsLinkedin />
+      </div>      
+      <div>
+        <BsGithub />
+      </div>      
+      <div>
+        <BsTwitter  />
+      </div>
+    </div>
+  );
+
+
+export default SocialMedia
